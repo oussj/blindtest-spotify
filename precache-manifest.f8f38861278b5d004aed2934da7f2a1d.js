69388d42ecd89763703dbc8822a18e844b5bc254
oussamah <oussamah.jaber@manomano.com>
2019-02-27 13:26:52 +0100

    Updates

---
+++ precache-manifest.f8f38861278b5d004aed2934da7f2a1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "bd4de7ae32d74b318af5",
+    "url": "/blindtest-spotify/static/js/runtime~main.bd4de7ae.js"
+  },
+  {
+    "revision": "d0ce1ebb023c9cd3f503",
+    "url": "/blindtest-spotify/static/js/main.d0ce1ebb.chunk.js"
+  },
+  {
+    "revision": "3f7525847cbf7fd2fe0c",
+    "url": "/blindtest-spotify/static/js/1.3f752584.chunk.js"
+  },
+  {
+    "revision": "d0ce1ebb023c9cd3f503",
+    "url": "/blindtest-spotify/static/css/main.b1a6cb8f.chunk.css"
+  },
+  {
+    "revision": "18cbf0e84281ab28ad002af2b3077124",
+    "url": "/blindtest-spotify/index.html"
+  }
+];
